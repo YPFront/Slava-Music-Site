adcc25970de630f075c19833c79b3831c4e95d08
Y <pecheniuky@gmail.com>
2021-09-06 12:10:14 +0300

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true
+};
+
+export const firebase = {
+  apiKey: 'AIzaSyDq8b_fkAa6yiYhxt3m1lsEH9R-6ioVIzE',
+  authDomain: 'zondimo-c553e.firebaseapp.com',
+  databaseURL: 'https://zondimo-c553e.firebaseio.com',
+  projectId: 'zondimo-c553e',
+  storageBucket: 'zondimo-c553e.appspot.com',
+  messagingSenderId: '93694676731'
+};
+
+// set this to true to use static data from "data" folder
+export const staticData = false;
